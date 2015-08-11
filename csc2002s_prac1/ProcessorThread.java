d95ac34ce6c91b7af43f7ada6e149922f3d609ca
Aidan de Nobrega <aidandeno@gmail.com>
2015-08-11 06:50:01 +0200

    Reorder files

---
+++ csc2002s_prac1/ProcessorThread.java
@@ -0,0 +1,45 @@
+
+/**
+ * Filters subarrays according to filtering type using standard threads
+ *
+ * @author Aidan de Nobrega DNBAID001
+ * @since 29/07/2015
+ */
+public class ProcessorThread extends Thread
+{
+    //******FIELDS******//
+
+    /**
+     * The lower index of the subarray to be filtered
+     */
+    private final int lo;
+
+    /**
+     * The upper index of the subarray to be filtered
+     */
+    private final int hi;
+
+    /**
+     * true -> median filtering false -> mean filtering
+     */
+    private final boolean filterType;
+
+    //******METHODS******//
+    public ProcessorThread(int lo, int hi, boolean filterType)
+    {
+        this.lo = lo;
+        this.hi = hi;
+        this.filterType = filterType;
+    }
+
+    @Override
+    /**
+     * Sequentially filters a subarray according to filtering type using
+     * standard threads
+     */
+    public void run()
+    {
+        FilterObject filt = new FilterObject(lo, hi, filterType);
+        filt.seqFilter();
+    }
+}
